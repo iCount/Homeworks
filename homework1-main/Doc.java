28fce81bb631a9ffd10eefb6b88d20fad5ab4f8a
Vanea B <75329633+iCount@users.noreply.github.com>
2022-09-10 13:21:03 +0300

    Add files via upload

---
+++ homework1-main/Doc.java
@@ -0,0 +1,8 @@
+public class Doc {
+    static int calculateDiv(int a, int b){
+        int y = 2;
+        System.out.println(y * 5 - 3);
+        return a / b;
+    }
+
+        }
---
+++ homework1-main/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 2;
+        System.out.println("x + y=" + (x + y));
+        System.out.println("x - y=" + (x - y));
+        System.out.println("x * y=" + (x * y));
+
+        short g = 3;
+        byte b = 2;
+        short d = (short) (g + b);
+        short h = (short) (g - b);
+        short j = (short) (g * b);
+        System.out.println(g + b);
+        System.out.println(g - b);
+        System.out.println(g * b);
+
+        int k = 2;
+        int l = 5-3;
+        System.out.println(k * l-3);
+
+
+        }
+    }
+
+

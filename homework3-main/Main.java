9e7d743f594f84050031c34bf5f674930e0830ca
Vanea B <75329633+iCount@users.noreply.github.com>
2022-09-10 13:22:24 +0300

    Add files via upload

---
+++ homework3-main/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 10, n;
+        n = in.nextInt();
+        for (i = 0; i < n; i+=2){
+            }
+        }
+    }
+
+
+
---
+++ homework3-main/dc.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class dc {
+    public static void main(String[] args) {
+        int c = 0;
+        Scanner in = new Scanner(System.in);
+        int i, n;
+        n = in.nextInt();
+        for (i = 0; i <= n; i+=1){
+            System.out.println(i);
+
+            if(i % 2 != 0){
+                array[c] = i;
+                c++;
+        }
+    }
+    
+    }
+}

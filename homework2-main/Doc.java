ebb7aaa8709ab6622f22e00760d5001cf1c9c74a
Vanea B <75329633+iCount@users.noreply.github.com>
2022-09-10 13:21:46 +0300

    Add files via upload

---
+++ homework2-main/Doc.java
@@ -0,0 +1,29 @@
+    public class Doc {
+        public static void main(String[] args) {
+            int x = 1;
+
+            switch (x) {
+                case 1:
+                    System.out.println("This is one");
+                    break;
+
+                    case 2:
+                        System.out.println("This is two");
+                        break;
+
+                        case 3:
+                            System.out.println("This is three");
+                            break;
+
+                            case 4:
+                                System.out.println("This is four");
+                                break;
+
+                                case 5:
+                                    System.out.println("This is five");
+                                    break;
+
+                            }
+                        }
+                    }
+
